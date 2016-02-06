54fe529c20467c92262e4214528b07f5f1ef9ea2
billyninja <joaoeduardocm@gmail.com>
2016-02-06 18:36:38 -0200

    setting up resty

--- puzzler/puzzler.go
+++ puzzler/puzzler.go
@@ -1,12 +1,16 @@
 package puzzler
 
 import (
-	/*"net/http"
-	"github.com/go-resty/resty"*/
+	"github.com/go-resty/resty"
 	"github.com/slapchop/actions"
 )
 
 
 func CreatePuzzle(actions.CreateResponse) {
+	resp, err := resty.R().
+    	SetHeader("Content-Type", "application/json").
+    	SetBody(`{"username":"testuser", "password":"testpass"}`).
+      	Post("http://localhost:8000")
+
 	return
 }
